7f7acbe23a16c8af6d1172d3fb22a0456872adde
QoderAI <qoder_ai@qoder.com>
2025-10-23 07:06:56 +0000

    feat: add addition functionality
    Co-authored-by: fei peng <>

---
+++ src/main/java/com/square/Addition.java
@@ -0,0 +1,8 @@
+package com.square;
+
+public class Addition {
+    public static void main(String[] args) {
+        int result = 44 + 55;
+        System.out.println("44 + 55 = " + result);
+    }
+}
